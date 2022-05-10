f70a9f269cbbcec762bef7da8c618e244f88968c
Joshua Davian <joshuadavian@gmail.com>
2022-05-10 13:16:11 +0700

    building ui

---
+++ resources/views/livewire/register.blade.php
@@ -0,0 +1,30 @@
+<div>
+    <div class="mb-3">
+        <label>Full Name</label>
+        <input type="text" class="form-control" name="name" placeholder="Full Name">
+        <div class="invalid-feedback">
+            Lorem, ipsum dolor.
+        </div>
+    </div>
+    <div class="mb-3">
+        <label>Email Address</label>
+        <input type="text" class="form-control" name="email" placeholder="user@example.com">
+        <div class="invalid-feedback">
+            Lorem, ipsum dolor.
+        </div>
+    </div>
+    <div class="mb-3">
+        <label>Password</label>
+        <input type="password" class="form-control" name="password" placeholder="Password">
+        <div class="invalid-feedback">
+            Lorem, ipsum dolor.
+        </div>
+    </div>
+    <div class="mb-3">
+        <label>Password Confirmation</label>
+        <input type="password" class="form-control" name="password_confirmation" placeholder="Email Address">
+    </div>
+    <button type="submit" class="btn btn-primary">
+        Register
+    </button>
+</div>
