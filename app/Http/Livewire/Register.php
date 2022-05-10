3fd6822b245baca85a56fc2c8cc2c6138efeb65a
Joshua Davian <joshuadavian@gmail.com>
2022-05-10 14:23:37 +0700

    realtime validation

--- app/Http/Livewire/Register.php
+++ app/Http/Livewire/Register.php
@@ -6,8 +6,21 @@ use Livewire\Component;
 
 class Register extends Component
 {
+    public $name, $email, $password, $password_confirmation;
+
+    protected $rules = [
+        'name' => ['required', 'string', 'max:255', 'min:3'],
+        'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+        'password' => ['required', 'string', 'min:8', 'confirmed'],
+    ];
+
     public function render()
     {
         return view('livewire.register');
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
     }
 }
--- resources/views/livewire/register.blade.php
+++ resources/views/livewire/register.blade.php
@@ -1,30 +1,46 @@
 <div>
-    <div class="mb-3">
-        <label>Full Name</label>
-        <input type="text" class="form-control" name="name" placeholder="Full Name">
-        <div class="invalid-feedback">
-            Lorem, ipsum dolor.
+    <form action="{{ route('register') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label>Full Name</label>
+            <input type="text" class="form-control @if($errors->has('name'))
+                is-invalid @elseif($name == null) @else is-valid
+            @endif" name="name" placeholder="Full Name" wire:model="name">
+            @error('name')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
         </div>
-    </div>
-    <div class="mb-3">
-        <label>Email Address</label>
-        <input type="text" class="form-control" name="email" placeholder="user@example.com">
-        <div class="invalid-feedback">
-            Lorem, ipsum dolor.
+        <div class="mb-3">
+            <label>Email Address</label>
+            <input type="text" class="form-control @if($errors->has('email'))
+                is-invalid @elseif($email == null) @else is-valid
+            @endif" name="email" placeholder="user@example.com" wire:model="email">
+            @error('email')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
         </div>
-    </div>
-    <div class="mb-3">
-        <label>Password</label>
-        <input type="password" class="form-control" name="password" placeholder="Password">
-        <div class="invalid-feedback">
-            Lorem, ipsum dolor.
+        <div class="mb-3">
+            <label>Password</label>
+            <input type="password" class="form-control @if($errors->has('password'))
+                is-invalid @elseif($password == null) @else is-valid
+            @endif" name="password" placeholder="Password" wire:model="password">
+            @error('password')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
         </div>
-    </div>
-    <div class="mb-3">
-        <label>Password Confirmation</label>
-        <input type="password" class="form-control" name="password_confirmation" placeholder="Email Address">
-    </div>
-    <button type="submit" class="btn btn-primary">
-        Register
-    </button>
+        <div class="mb-3">
+            <label>Password Confirmation</label>
+            <input type="password" class="form-control" name="password_confirmation" placeholder="Email Address"
+                wire:model="password_confirmation">
+        </div>
+        <button type="submit" class="btn btn-primary">
+            Register
+        </button>
+    </form>
 </div>
